712a43f569180924db2ad4932aee7dc3d0444a4e
Steven Zhu <stevenzhu03@gmail.com>
2020-08-13 17:42:24 -0400

    implemented a get, set, and event handler in User class

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,13 +1,28 @@
 //interface to describe a user's properties
 interface UserProps {
-  name: string;
-  age: number;
+  // ? makes these properties optional
+  name?: string;
+  age?: number;
 }
 
+type Callback = () => void;
+
 export class User {
+  events: { [key: string]: Callback[] } = {};
+
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
     return this.data[propName];
+  }
+
+  set(updateProps: UserProps): void {
+    Object.assign(this.data, updateProps);
+  }
+
+  on(eventName: string, callback: Callback) {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
   }
 }
