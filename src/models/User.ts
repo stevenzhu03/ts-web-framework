cc8861c3d09120a65d5f29e2199ec2efed13b0cc
Steven Zhu <stevenzhu03@gmail.com>
2020-08-13 17:49:17 -0400

    implemented prelim event system for a User object

--- src/models/User.ts
+++ src/models/User.ts
@@ -12,17 +12,31 @@ export class User {
 
   constructor(private data: UserProps) {}
 
+  //gets info for the user
   get(propName: string): number | string {
     return this.data[propName];
   }
 
+  //updates info about the user
   set(updateProps: UserProps): void {
     Object.assign(this.data, updateProps);
   }
 
+  //registers an event handler with this user object
   on(eventName: string, callback: Callback) {
     const handlers = this.events[eventName] || [];
     handlers.push(callback);
     this.events[eventName] = handlers;
+  }
+
+  //trigger event to alert other parts of app something has changed
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) return;
+
+    handlers.forEach((callback) => {
+      callback();
+    });
   }
 }
