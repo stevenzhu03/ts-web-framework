18f4522f6ce4e2f34b579d40813146ff93a73a3e
Steven Zhu <stevenzhu03@gmail.com>
2020-08-19 00:40:11 -0400

    implementing Attribute class in User

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
+import { Attributes } from "./Attributes";
 
 //interface to describe a user's properties
 export interface UserProps {
@@ -14,6 +15,9 @@ const rootUrl = "http://localhost:3000/users";
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
 
-  //gets info for the user
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
+  }
 }
