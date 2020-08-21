deab57d7fbb797757fad1b46c4b833eda515cb96
Steven Zhu <stevenzhu03@gmail.com>
2020-08-21 17:25:31 -0400

    moved methods from User to Model class

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -1,4 +1,4 @@
-import { AxiosPromise } from "axios";
+import { AxiosPromise, AxiosResponse } from "axios";
 
 interface ModelAttributes<T> {
   set(value: T): void;
@@ -16,4 +16,59 @@ interface Events {
   trigger(eventName: string): void;
 }
 
-export class Model {}
+interface HasId {
+  id?: number;
+}
+
+export class Model<T extends HasId> {
+  constructor(
+    private attributes: ModelAttributes<T>,
+    private events: Events,
+    private sync: Sync<T>
+  ) {}
+
+  //pass-through methods (immediatly invokes methods in other class)
+  //accesssor (getter)
+  get on() {
+    //returns the reference to the on method in Eventing, not calling it
+    //you can use by calling user.on("change", .....callback)
+    return this.events.on;
+  }
+
+  //accesssor (getter)
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  //accesssor (getter)
+  get get() {
+    return this.attributes.get;
+  }
+
+  set(update: T): void {
+    this.attributes.set(update);
+    //user info has change, probably want to modify some HTML
+    this.events.trigger("change");
+  }
+
+  fetch(): void {
+    const id = this.attributes.get("id");
+
+    if (typeof id !== "number") throw new Error("Cannot fetch without and id");
+
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+  }
+
+  save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((response: AxiosResponse) => {
+        this.trigger("save");
+      })
+      .catch(() => {
+        this.trigger("error");
+      });
+  }
+}
--- src/models/User.ts
+++ src/models/User.ts
@@ -21,49 +21,4 @@ export class User {
   constructor(attrs: UserProps) {
     this.attributes = new Attributes<UserProps>(attrs);
   }
-
-  //pass-through methods (immediatly invokes methods in other class)
-  //accesssor (getter)
-  get on() {
-    //returns the reference to the on method in Eventing, not calling it
-    //you can use by calling user.on("change", .....callback)
-    return this.events.on;
-  }
-
-  //accesssor (getter)
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  //accesssor (getter)
-  get get() {
-    return this.attributes.get;
-  }
-
-  set(update: UserProps): void {
-    this.attributes.set(update);
-    //user info has change, probably want to modify some HTML
-    this.events.trigger("change");
-  }
-
-  fetch(): void {
-    const id = this.attributes.get("id");
-
-    if (typeof id !== "number") throw new Error("Cannot fetch without and id");
-
-    this.sync.fetch(id).then((response: AxiosResponse): void => {
-      this.set(response.data);
-    });
-  }
-
-  save(): void {
-    this.sync
-      .save(this.attributes.getAll())
-      .then((response: AxiosResponse) => {
-        this.trigger("save");
-      })
-      .catch(() => {
-        this.trigger("error");
-      });
-  }
 }
