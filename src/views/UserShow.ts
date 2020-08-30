3a11d50133ccc231c8c2c7e4991fb24a625f5545
Steven Zhu <stevenzhu03@gmail.com>
2020-08-29 23:59:46 -0400

    removed unecessary HTML from template method in UserShow

--- src/views/UserShow.ts
+++ src/views/UserShow.ts
@@ -8,10 +8,6 @@ export class UserShow extends View<User, UserProps> {
                 <h1>User Form</h1>
                 <div>User name: ${this.model.get("name")}</div>
                 <div>User age: ${this.model.get("age")}</div>
-                <input/>
-                <button class="set-name">Change Name</button>
-                <button class="set-age">Set Random Age</button>
-                <button class="save-model">Save</button>
             </div>
         `;
   }
