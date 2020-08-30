4b7fc37a0d117765ada89925386a945a07e8f832
Steven Zhu <stevenzhu03@gmail.com>
2020-08-29 23:57:26 -0400

    refactoring user information to UserShow and UserEdit class

--- src/views/UserShow.ts
+++ src/views/UserShow.ts
@@ -1,4 +1,18 @@
 import { View } from "./View";
 import { User, UserProps } from "../models/User";
 
-export class UserShow extends View<User, UserProps> {}
+export class UserShow extends View<User, UserProps> {
+  template(): string {
+    return `
+            <div>
+                <h1>User Form</h1>
+                <div>User name: ${this.model.get("name")}</div>
+                <div>User age: ${this.model.get("age")}</div>
+                <input/>
+                <button class="set-name">Change Name</button>
+                <button class="set-age">Set Random Age</button>
+                <button class="save-model">Save</button>
+            </div>
+        `;
+  }
+}
